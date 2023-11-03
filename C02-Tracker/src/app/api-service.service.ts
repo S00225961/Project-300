848634c3b0746a8fb014ed6dd62243ec7fcb01cd
GL_point <s00225961@atu.ie>
2023-11-03 18:44:03 +0000

    updated favicon

--- C02-Tracker/src/app/api-service.service.ts
+++ C02-Tracker/src/app/api-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  constructor() { }
+  private apiUrl = 'https://world.openfoodfacts.org/api/v2/search'; 
+
+  constructor(private http: HttpClient) {}
+
+  searchItems(query: string): Observable<any> {
+    const url = `${this.apiUrl}?query=${query}`;
+    return this.http.get(url);
+  }
 }
