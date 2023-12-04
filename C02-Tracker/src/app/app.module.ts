0b546e6729cbad8d336d75bfd4d88a0fe1bcdbf1
WarGooglee <s00227303@mail.itsligo.ie>
2023-12-04 23:23:55 +0000

    Basic forms and calculation logic implemented

--- C02-Tracker/src/app/app.module.ts
+++ C02-Tracker/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { DisplayProductsComponent } from './display-products/display-products.component';
 import { CommuteTrackerComponent } from './commute-tracker/commute-tracker.component';
 import { ElectricityTrackerComponent } from './electricity-tracker/electricity-tracker.component';
-import { HomeComponent } from './home/home.component'; 
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +25,10 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- C02-Tracker/src/app/electricity-tracker/electricity-tracker.component.ts
+++ C02-Tracker/src/app/electricity-tracker/electricity-tracker.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-electricity-tracker',
   templateUrl: './electricity-tracker.component.html',
   styleUrls: ['./electricity-tracker.component.css']
 })
-export class ElectricityTrackerComponent {
+export class ElectricityTrackerComponent implements OnInit {
+  electricityForm: FormGroup = new FormGroup({});
+
+  co2Produced: number | null = null;
+
+  co2ConversionFactor = 0.5;
+
+  electricitySources = [
+    { value: 'coal', label: 'Coal' },
+    { value: 'naturalGas', label: 'Natural Gas' },
+    { value: 'renewable', label: 'Renewable Energy' },
+    { value: 'other', label: 'Other' }
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.electricityForm = this.fb.group({
+      usageInKwh: ['', [Validators.required, Validators.min(0)]],
+      timeUsedInHours: ['', [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  calculateCO2(): number {
+    if (this.electricityForm.valid) {
+      const formData = this.electricityForm.value;
+      const { usageInKwh, timeUsedInHours } = formData;
+
+      this.co2Produced = usageInKwh * timeUsedInHours * this.co2ConversionFactor;
+
+      return this.co2Produced;
+    }
+
+    return -1;
+  }
+
+  onSubmit(): void {
+    this.co2Produced = this.calculateCO2();
+
+    if (this.co2Produced !== -1) {
+      console.log(`CO2 produced: ${this.co2Produced} kg`);
+    } else {
+      console.error('Invalid input. Please check your values.');
+    }
+  }
 
 }
