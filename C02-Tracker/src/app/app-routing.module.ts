aeb4080892fc278efc5e3cfc4719eb7370861f2c
Jack <s00227213@atu.ie>
2023-12-04 13:26:05 +0000

    Add HomeComponent and adjust routing and module declarations

--- C02-Tracker/src/app/app-routing.module.ts
+++ C02-Tracker/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; 
 import { ElectricityTrackerComponent } from './electricity-tracker/electricity-tracker.component';
 import { CommuteTrackerComponent } from './commute-tracker/commute-tracker.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
-  {path: '/electricity', component: ElectricityTrackerComponent},
-  {path: '/commute', component: CommuteTrackerComponent}
+  { path: 'home', component: HomeComponent }, 
+  { path: 'electricity', component: ElectricityTrackerComponent },
+  { path: 'commute', component: CommuteTrackerComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
 ];
 
 @NgModule({
--- C02-Tracker/src/app/app.module.ts
+++ C02-Tracker/src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { DisplayProductsComponent } from './display-products/display-products.component';
 import { CommuteTrackerComponent } from './commute-tracker/commute-tracker.component';
 import { ElectricityTrackerComponent } from './electricity-tracker/electricity-tracker.component';
-
+import { HomeComponent } from './home/home.component'; 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
     DisplayProductsComponent,
     CommuteTrackerComponent,
-    ElectricityTrackerComponent
+    ElectricityTrackerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
